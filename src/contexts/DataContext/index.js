dbe855d3643c3cc5486c80585ce8efa0bbabfd0a
Allix Dolou <dolou.allix@gmail.com>
2025-03-09 16:42:41 +0100

    First commit, last event update

---
+++ src/contexts/DataContext/index.js
@@ -0,0 +1,61 @@
+import PropTypes from "prop-types";
+import {
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+
+const DataContext = createContext({});
+
+export const api = {
+  loadData: async () => {
+    const json = await fetch("/events.json");
+    return json.json();
+  },
+};
+
+export const DataProvider = ({ children }) => {
+  const [error, setError] = useState(null);
+  const [data, setData] = useState(null);
+  const [last, setLast] = useState(null); // Distinct state for `last`
+
+  const getData = useCallback(async () => {
+    try {
+      const result = await api.loadData();
+      setData(result);
+      setLast(result?.last || null); // `last` update when `data` change
+    } catch (err) {
+      setError(err);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (!data) {
+      getData();
+    }
+  }, [data, getData]); // Dependencies update
+
+  return (
+    <DataContext.Provider
+      // eslint-disable-next-line react/jsx-no-constructed-context-values
+      value={{
+        data,
+        error,
+        last, // `last` is defined correctly
+      }}
+    >
+      {children}
+    </DataContext.Provider>
+  );
+};
+
+DataProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export const useData = () => useContext(DataContext);
+
+export default DataContext;
+
